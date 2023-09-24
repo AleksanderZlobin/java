d788c2aac3d27669afbd10ad73270e53aa60cd86
AleksanderZlobin <lancer111187@mail.ru>
2023-09-24 18:54:06 +0700

    add some program

---
+++ program.java
@@ -0,0 +1,29 @@
+
+/*
+ * 
+ * 
+ * 
+ */
+public class program {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+class BubbleSort {
+    private static File log;
+    private static FileWriter fileWriter;
+
+    public static void sort(int[] mas) {
+
+    }
+}
---
+++ program2.java
@@ -0,0 +1,32 @@
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+class BubbleSort {
+    private static File log;
+    private static FileWriter fileWriter;
+
+    public static void sort(int[] mas) {
+        try {
+            log = new File("log.txt");
+            log.createNewFile();
+            fileWriter = new FileWriter(log);
+            bubbleSort(mas);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
